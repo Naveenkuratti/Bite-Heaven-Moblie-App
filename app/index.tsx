9ab473aad373e7c194938338d7fd7ac36c6bb8cd
kotappa19 <kgandudi@gmail.com>
2025-01-23 20:07:49 +0530

    Initial Setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-blue-500 font-bold">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
