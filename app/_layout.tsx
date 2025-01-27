feb2bf602a3dd7bd8235ef5974fb925d25e5f99e
Naveen Kuratti <naveenkuratti875@gmail.com>
2025-01-27 20:30:46 +0530

    send main files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,19 @@
 import { Stack } from "expo-router";
-import '../global.css'
 
-export default function RootLayout() {
-  return <Stack />;
-}
+
+const Layout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="addrestaurant" />
+      <Stack.Screen name="loginin" />
+      <Stack.Screen name="register" />
+      <Stack.Screen name="Registerrestaurant" />
+      <Stack.Screen name="splashScreen" />
+      
+      
+    </Stack>
+  );
+};
+
+export default Layout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from "react-native";
 
-export default function Index() {
+
+import Header from "@/components/Header";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native";
+import AddRestratant from './Button/AddRestratant';
+import Registernow from './Button/Registernow';
+
+export default function index() {
+  const router = useRouter();
+
+  const navigateToAddRestaurant = () => {
+    router.navigate("./Button/AddRestratant"); // Correct navigation for Add Restaurant
+  };
+
+  const navigateToLogin = () => {
+    router.navigate("./Button/Login"); // Correct navigation for LoginIn
+  };
+  
   return (
-    <View>
-      <Text className="text-blue-500 font-bold">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+
+      
+      <Header
+        title="Add Restaurant"
+        onPress={navigateToAddRestaurant}
+        navigation={{ navigateToLogin }}
+
+      />
+
+    </SafeAreaView>
   );
 }
