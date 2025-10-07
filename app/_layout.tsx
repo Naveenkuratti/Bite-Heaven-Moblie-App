83a042db7fbebb6077d519aca218b3b610bd9f39
Naveen Kuratti <naveenkuratti875@gmail.com>
2025-10-07 08:41:33 +0530

    updated code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,4 @@
-// import { Stack } from "expo-router";
 
-
-// const Layout = () => {
-//   return (
-//     <Stack screenOptions={{ headerShown: false }}>
-//       <Stack.Screen name="index" />
-//       <Stack.Screen name="addrestaurant" />
-//       <Stack.Screen name="loginin" />
-//       <Stack.Screen name="register" />
-//       <Stack.Screen name="Registerrestaurant" />
-//       <Stack.Screen name="splashScreen" />
-      
-      
-//     </Stack>
-//   );
-// };
-
-// export default Layout;
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
